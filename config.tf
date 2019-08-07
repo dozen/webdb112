2cd121bcaa220eb42025a5371d3368024fb68068
Suzuki Kyosuke <suzuki-kyosuke@kayac.com>
2019-08-07 20:53:54 +0900

    tfstateを暗号化してS3に保管する

--- config.tf
+++ config.tf
@@ -5,4 +5,12 @@ provider "aws" {
 
 terraform {
   required_version = "= 0.12.1"
+
+  backend "s3" {
+    bucket     = "terraform.example.com" # 作成したS3バケット名で置き換える
+    key        = "terraform.tfstate"
+    region     = "ap-northeast-1"
+    encrypt    = true
+    kms_key_id = "arn:aws:kms:ap-northeast-1:111111111111:key/44757374-2069-6e20-74686-52077696e64" # 発行したCMKで置き換える
+  }
 }
